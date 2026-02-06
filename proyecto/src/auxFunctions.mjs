183504c5e023fab9b0285d828e33f14bd602e892
Pedro Prieto <pedpral@gmail.com>
2026-02-06 13:36:49 +0100

    Fase 1

--- proyecto/src/auxFunctions.mjs
+++ proyecto/src/auxFunctions.mjs
@@ -1,10 +1,16 @@
-// Clientes para interactuar con la API de AWS
+// Importación de librerías
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import {
   DynamoDBDocumentClient,
   QueryCommand,
   PutCommand,
 } from "@aws-sdk/lib-dynamodb";
+import { PollyClient, SynthesizeSpeechCommand } from "@aws-sdk/client-polly";
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+
+// TODO: importar librerías adicionales (Translate)
+
+// Clientes para interactuar con la API de DynamoDB
 const client = new DynamoDBClient({});
 const ddbDocClient = DynamoDBDocumentClient.from(client);
 
@@ -44,4 +50,48 @@ async function postNoteForUser(userId, noteId, noteText) {
   return data;
 }
 
-export { getNotesByUser, postNoteForUser };
+// Función que recibe un texto de una nota y devuelve un buffer con los datos sintetizados por Polly
+async function textToSpeech(text) {
+  const pollyClient = new PollyClient();
+  const command = new SynthesizeSpeechCommand({
+    Text: text,
+    OutputFormat: "mp3",
+    VoiceId: "Joanna", // Puedes cambiar este valor si lo deseas. Consulta la doc de Polly
+  });
+
+  const response = await pollyClient.send(command);
+  const audioStream = response.AudioStream;
+
+  // Convertir a buffer
+  const chunks = [];
+  for await (const chunk of audioStream) {
+    chunks.push(chunk);
+  }
+
+  return Buffer.concat(chunks);
+}
+
+// Función que recibe un buffer con los datos sintetizados por Polly y los almacena en el objeto con nombre "key" en S3
+async function uploadToS3(mp3Data, key) {
+  const s3Client = new S3Client();
+
+  // Obtener el nombre del bucket S3 a partir de la variable de entorno
+  const bucketName = process.env.APP_S3;
+  const command = new PutObjectCommand({
+    Bucket: bucketName,
+    Key: key,
+    Body: mp3Data,
+    ContentType: "audio/mpeg",
+  });
+
+  await s3Client.send(command);
+
+  // TODO: modificar para devolver una URL prefirmada de S3 que permita descargar
+  // el audio durante un tiempo limitado de 5 minutos
+  return;
+}
+
+// TODO: Añadir el resto de funciones necesarias de lógica de negocio
+
+// TODO: Exportar las funciones creadas
+export { getNotesByUser, postNoteForUser, textToSpeech, uploadToS3 };
